40b9ed7a338ba0d82bc7ac52916c7d73a26eb4a1
freecssli <23058006612@qq.com>
2026-05-16 17:20:45 +0800

    fix: restore index.js entry point

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import { registerRootComponent } from 'expo';
-
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// 这一行告诉 Expo，App.js 是你的应用入口
 registerRootComponent(App);
